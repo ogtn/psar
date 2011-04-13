4e022a15802a1de510e9fe9a6b0260fd87502130
antoine.blin3@gmail.com <antoine.blin3@gmail.com@3198b614-173f-edcd-ee22-36cd6a9b0a7e>
2011-04-13 21:58:04 +0000

    Test unitaires du range (Un des test fail) Plus commentaires du range.

--- trunk/src/dht/Range.java
+++ trunk/src/dht/Range.java
@@ -1,9 +1,12 @@
 package dht;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.Map.Entry;
+import java.util.Map;
 import java.util.TreeMap;
+import java.util.Map.Entry;
 
 public class Range {
 
@@ -39,14 +42,20 @@ public class Range {
 	}
 
 	public final static long MAX_KEY = 4294967296L;
-
+	
 	private final Longer begin, end;
-	private TreeMap<Long, Object> data;
+	private final TreeMap<Long, Object> data;
+	private final Map<Long, Object> unmodifiableData;
 
-	/* Empty range */
-	public Range(long id, boolean isNotAlone) {
-		checkUnsignedInt(id);
-		begin = new Longer(id);
+	/**
+	 * Crée et initialise une plage de données vide.
+	 * 
+	 * @param key
+	 *            La clé utilisée pour le début de la plage.
+	 */
+	public Range(long key, boolean isNotAlone) {
+		checkUnsignedInt(key);
+		begin = new Longer(key);
 		end = new Longer(-1);
 
 		data = new TreeMap<Long, Object>(new Comparator<Long>() {
@@ -63,38 +72,43 @@ public class Range {
 					keyTwo += MAX_KEY;
 
 				return keyOne - keyTwo < 0 ? -1 : 1;
-
 			}
 		});
+
+		unmodifiableData = Collections.unmodifiableMap(data);
 	}
 
-	public Range(long id) {
-		this(id, true);
-		begin.longer = id;
-		end.longer = (id - 1 + MAX_KEY) % MAX_KEY;
+	/**
+	 * Crée et initialise une plage de "MAX_KEY" données.
+	 * 
+	 * @param key
+	 *            La clé utilisée pour le début de la plage.
+	 */
+	public Range(long key) {
+		this(key, true);
+		begin.longer = key;
+		end.longer = (key - 1 + MAX_KEY) % MAX_KEY;
 	}
 
-	boolean inRange(long key) {
-
-		/*
-		 * checkUnsignedInt(key);
-		 * 
-		 * 
-		 * // Plage vide if (end.longer == -1) return false;
-		 * 
-		 * // Pas de bouclage if (begin.longer <= end.longer) return key >=
-		 * begin.longer && key <= end.longer;
-		 * 
-		 * // Bouclage : comparaison en deux parties if (key >= begin.longer &&
-		 * key < MAX_KEY) return true; if (key >= 0 && key <= end.longer) return
-		 * true;
-		 * 
-		 * return false;
-		 */
-
-		return inRange(begin.longer, end.longer, key);
+	private static void checkUnsignedInt(long key) {
+		if (key < 0)
+			throw new IndexOutOfBoundsException("negativ key : " + key);
+		if (key > MAX_KEY)
+			throw new IndexOutOfBoundsException("key too large");
 	}
 
+	/**
+	 * Test si une clé est dans une plage de données.
+	 * 
+	 * @param begin
+	 *            Le début de la plage.
+	 * @param end
+	 *            La fin de la plage.
+	 * @param key
+	 *            La clé à tester.
+	 * @return <code>true</code> si la clé est dans la plage de données,
+	 *         <code>false</code> sinon.
+	 */
 	private static boolean inRange(long begin, long end, long key) {
 		checkUnsignedInt(begin);
 		checkUnsignedInt(end);
@@ -117,135 +131,33 @@ public class Range {
 		return false;
 	}
 
-	Data shrinkToLast(long key) {
-		checkUnsignedInt(key);
-
-		if (data.size() == 0)
-			return null;
-		else if (end.longer == -1)
-			throw new IllegalStateException("Plage invalide:" + this);
-
-		Long tmpKey = data.lastKey();
-
-		Data res = null;
-		if (data.comparator().compare(tmpKey, key) >= 0) {
-			res = new Data(tmpKey, data.remove(tmpKey));
-			shrinkEnd(tmpKey);
-		}
-
-		return res;
-	}
-
-	void add(long key, Object data) {
-		if (!inRange(key))
-			throw new IndexOutOfBoundsException("Ajout impossible: la clé "
-					+ key + " n'est pas dans le " + toString());
-
-		this.data.put(key, data);
-	}
-
-	void addExtend(long key, Object data) {
-		if (inRange(key) == false)
-			end.longer = key;
-
-		this.data.put(key, data);
-	}
-
-	Object get(long key) {
-		checkUnsignedInt(key);
-		return data.get(key);
-	}
-
-	private static void checkUnsignedInt(long key) {
-		if (key < 0)
-			throw new IndexOutOfBoundsException("negativ key : " + key);
-		if (key > MAX_KEY)
-			throw new IndexOutOfBoundsException("key too large");
-	}
-
-	@Override
-	public String toString() {
-		return "range[" + begin + ":" + end + "| data: " + data.size() + "]";
-	}
-
-	// TODO iter lecture
-	public Iterator<Entry<Long, Object>> iterator() {
-		return data.entrySet().iterator();
-	}
-
-	/*
-	 * Aux chiottes Junit \o/
+	/**
+	 * Test si la plage de données courante est valide.
+	 * 
+	 * @return <true> si la plage est valide, <false> sinon.
 	 */
-	public static void main(String[] args) {
-		Range range = new Range(450);
-
-		range.addExtend(1024, 1024);
-		range.add(822, 822);
-		range.add(51, 51);
-		range.add(78978, 78978);
-		range.add(450, 450);
-		range.add(449, 449);
-		System.out.println(range);
-
-		try {
-			range.setBegin(451);
-		} catch (IndexOutOfBoundsException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			range.setEnd(448);
-		} catch (IndexOutOfBoundsException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			range.shrinkEnd(51);
-		} catch (IndexOutOfBoundsException e) {
-			e.printStackTrace();
-		}
-
-		System.out.println("\n========================");
-		Iterator<Entry<Long, Object>> iter = range.iterator();
-		while (iter.hasNext())
-			System.out.println(" [" + iter.next() + "] ");
-
-		System.out.println(range);
-
-		/*
-		 * System.out.println("\n========================");
-		 * 
-		 * System.out.println("removeLast(500) : " + range.removeLast(500));
-		 * 
-		 * iter = range.iterator(); while (iter.hasNext())
-		 * System.out.println(" [" + iter.next() + "] ");
-		 * 
-		 * System.out.println("\n========================");
-		 * 
-		 * System.out.println("removeLast(500) : " + range.removeLast(500));
-		 * 
-		 * iter = range.iterator();
-		 * 
-		 * while (iter.hasNext()) System.out.println(" [" + iter.next() + "] ");
-		 * 
-		 * System.out.println("\n========================");
-		 * 
-		 * System.out.println("removeLast(500) : " + range.removeLast(500));
-		 * 
-		 * iter = range.iterator();
-		 * 
-		 * while (iter.hasNext()) System.out.println(" [" + iter.next() + "] ");
-		 * System.out.println("\n========================");
-		 * 
-		 * System.out.println("removeLast(500) : " + range.removeLast(500));
-		 */
+	private boolean isValidRange() {
+		return end.longer != -1;
 	}
 
-	private void check(long newBegin, long newEnd) {
+	/**
+	 * Vérifie si l'agrandissement de la plage de données courantes ne pose pas
+	 * de problème.
+	 * 
+	 * @param newBegin
+	 *            Le nouveau début de la plage.
+	 * @param newEnd
+	 *            La nouvelle fin de la plage.
+	 * @throws IndexOutOfBoundsException
+	 *             Une exception est lancée si la plage est invalide.
+	 */
+	private void check(long newBegin, long newEnd)
+			throws IndexOutOfBoundsException {
+
 		checkUnsignedInt(newEnd);
 		checkUnsignedInt(newBegin);
 
-		if (data.isEmpty())
+		if (data.isEmpty() || isValidRange() == false)
 			return;
 
 		if (inRange(newBegin, newEnd, data.firstKey()) == false)
@@ -258,28 +170,187 @@ public class Range {
 					+ "]");
 	}
 
-	public long getEnd() {
+	/**
+	 * Test si une clé est dans la plage de données courante.
+	 * 
+	 * @param key
+	 *            La clé à tester.
+	 * @return <code>true</code> si la clé est dans la plage de données,
+	 *         <code>false</code> sinon.
+	 */
+	boolean inRange(long key) {
+		return inRange(begin.longer, end.longer, key);
+	}
+
+	/**
+	 * Ajoute une donnée dans la plage.
+	 * 
+	 * @param key
+	 *            La clé de la donnée.
+	 * @param data
+	 *            La donnée à ajouter.
+	 */
+	void add(long key, Object data) {
+		if (!inRange(key))
+			throw new IndexOutOfBoundsException("Ajout impossible: la clé "
+					+ key + " n'est pas dans le " + toString());
+
+		this.data.put(key, data);
+	}
+
+	/**
+	 * Ajoute une donnée à la fin de la plage, en l'étendant si nécessaire.
+	 * 
+	 * @param key
+	 *            La clé de la donnée à ajouter.
+	 * @param data
+	 *            La donnée à ajouter.
+	 */
+	void addExtend(long key, Object data) {
+		if (inRange(key) == false)
+			end.longer = key;
+
+		this.data.put(key, data);
+	}
+
+	/**
+	 * Recherche la donnée correspondante à la clé.
+	 * 
+	 * @param key
+	 *            La clé de la donnée recherchée.
+	 * @return La donnée recherchée ou <code>null</code> si aucune donnée ne
+	 *         correspond à la clé.
+	 */
+	Object get(long key) {
+		checkUnsignedInt(key);
+		return data.get(key);
+	}
+
+	/**
+	 * Retourne une vue non modifiable des données stockées dans la plage.
+	 * 
+	 * @return une vue non modifiable des données.
+	 */
+	Map<Long, Object> getData() {
+		return unmodifiableData;
+	}
+
+	/**
+	 * <p>
+	 * Rétrécit la fin de la plage de données jusqu'à la clé inclue.
+	 * </p>
+	 * 
+	 * Exemple :
+	 * <ul>
+	 * <li>Soit la plage des données suivantes [100 à 500]</li>
+	 * <li>range.shrinkEnd(300) donne la plage [100 à 299]</li>
+	 * </li>
+	 * </ul>
+	 * 
+	 * @param end
+	 *            La nouvelle fin de la plage de données (fin exclue).
+	 * @throws IndexOutOfBoundsException
+	 *             Une exception est lancée si on tente de rétrécir la plage
+	 *             alors qu'une donnée est présente dans l'intervalle rétrécit.
+	 */
+	void shrinkEnd(long end) throws IndexOutOfBoundsException {
+		long tmpEnd = (end - 1 + MAX_KEY) % MAX_KEY;
+		check(begin.longer, tmpEnd);
+		this.end.longer = tmpEnd;
+	}
+
+	/**
+	 * <p>
+	 * Retire une donnée de la plage, si sa clé est plus grande que celle passée
+	 * en paramètre.
+	 * 
+	 * Si une donnée existe, la plage de données est alors rétrécie jusqu'à la
+	 * clé de la donnée inclue et la donnée est retournée.
+	 * 
+	 * Si aucune donnée n'a une clé supérieure à celle passée en paramètre, la
+	 * valeur <code>null</code> est retournée et la plage de données n'est pas
+	 * modifiée.
+	 * </p>
+	 * 
+	 * Exemple :
+	 * <ul>
+	 * <li>Soit la plage des données suivantes [100 à 500].</li>
+	 * <li>Soit les données ayant les clés 200 250 300.</li>
+	 * <li>shrinkToLast(250) retourne la donnée 300 et donne la plage [100 à
+	 * 299]</li>
+	 * <li>shrinkToLast(250) retourne la donnée 250 et donne la plage [100 à
+	 * 249]</li>
+	 * <li>shrinkToLast(200) retourne <code>null</code> et donne la plage [100 à
+	 * 249]</li>
+	 * </li>
+	 * </ul>
+	 * 
+	 * @param key
+	 *            La clé après laquelle on retire les données plus grandes.
+	 * @return La donnée retirée ou <code>null</code> si il y'en a pas.
+	 */
+	Data shrinkToLast(long key) {
+		checkUnsignedInt(key);
+
+		if (data.size() == 0 || isValidRange() == false)
+			return null;
+
+		Long tmpKey = data.lastKey();
+		Data res = null;
+
+		if (data.comparator().compare(tmpKey, key) >= 0) {
+			res = new Data(tmpKey, data.remove(tmpKey));
+			shrinkEnd(tmpKey);
+		}
+
+		return res;
+	}
+
+	/**
+	 * Retourne la fin de la plage.
+	 * 
+	 * @return La fin de la plage.
+	 */
+	long getEnd() {
 		return end.longer;
 	}
 
-	public void shrinkEnd(long id) {
-		long tmpEnd = (id - 1 + MAX_KEY) % MAX_KEY;
-		check(begin.longer, tmpEnd);
-		end.longer = tmpEnd;
-	}
-
-	public void setEnd(long end) {
+	/**
+	 * Modifie la fin de la plage.
+	 * 
+	 * @param end
+	 *            La nouvelle fin de la plage.
+	 */
+	void setEnd(long end) {
 		check(begin.longer, end);
 		this.end.longer = end;
 	}
 
-	public void setBegin(long begin) {
+	/**
+	 * Retourne le début de la plage.
+	 * 
+	 * @return Le nouveau début de la plage.
+	 */
+	long getBegin() {
+		return begin.longer;
+	}
+
+	/**
+	 * Modifie le début de la plage.
+	 * 
+	 * @param begin
+	 *            Le nouveau début de la plage.
+	 */
+	void setBegin(long begin) {
 		check(begin, end.longer);
 		this.begin.longer = begin;
 	}
 
-	public long getBegin() {
-		// TODO Auto-generated method stub
-		return begin.longer;
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String toString() {
+		return "range[" + begin + ":" + end + "| data: " + data.size() + "]";
 	}
 }
