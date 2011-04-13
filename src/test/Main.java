9dab5107d193e843ff88507c1b3dd569334dba90
antoine.blin3@gmail.com <antoine.blin3@gmail.com@3198b614-173f-edcd-ee22-36cd6a9b0a7e>
2011-04-13 18:01:59 +0000

    Classe de test du socketChannel

---
+++ src/test/Main.java
@@ -0,0 +1,22 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		Server s = new Server();
+		s.start();
+
+		Thread.sleep(500);
+
+		Client c1 = new Client();
+		c1.start();
+		c1.join();
+		
+		Client c2 = new Client();
+		c2.start();
+		c2.join();
+		
+		s.join();
+	}
+}
