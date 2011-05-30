3db7108d051b2662d466f55a07309191dd8a656f
OpenMEngine@gmail.com <OpenMEngine@gmail.com@3198b614-173f-edcd-ee22-36cd6a9b0a7e>
2011-05-30 14:04:54 +0000

    prout

--- trunk/src/dht/INetworkListener.java
+++ trunk/src/dht/INetworkListener.java
@@ -5,6 +5,6 @@ import java.util.EventListener;
 import dht.message.AMessage;
 
 public interface INetworkListener extends EventListener {
-	void sendMessage(AMessage message, INode node, ANodeId id, boolean isInChannel);
-	void recvMessage(AMessage message, INode node);
+	void eventSendMessage(AMessage message, INode node, ANodeId id, boolean isInChannel);
+	void eventRecvMessage(AMessage message, INode node); // TODO modifier l'heritage pour un nom conforme au rapport
 }
