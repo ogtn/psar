6b40eea327fa5eb4f5326eb5184a2fe1e47ec168
OpenMEngine@gmail.com <OpenMEngine@gmail.com@3198b614-173f-edcd-ee22-36cd6a9b0a7e>
2011-06-04 10:25:29 +0000

    prout

--- trunk/lancement_rep/monitor.c
+++ trunk/lancement_rep/monitor.c
@@ -1,5 +1,4 @@
 #include <SDL/SDL.h>
-#include <SDL/SDL_image.h>
 #include <stdio.h>
 
 int main(int argc, char **argv)
@@ -8,32 +7,32 @@ int main(int argc, char **argv)
     SDL_Event event;
     SDL_Surface *screen = NULL;
     SDL_Surface *img = NULL;
+    SDL_Surface *img2 = NULL;
     int i = 0;
     
     SDL_Init(SDL_INIT_VIDEO);
     screen = SDL_SetVideoMode(1024, 1024, 32, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_RESIZABLE);
     
-    while("true")
+    do
     {
         if(SDL_PollEvent(&event))
             if(event.type == SDL_QUIT || event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)
                 break;
-                        
-        system("circo graph.txt -Tjpg -o graph.jpg");
-            
-        if(img != NULL)
-            SDL_FreeSurface(img);
-            
-        if((img = IMG_Load("graph.jpg")) == NULL)
-            puts(SDL_GetError());
+        
+        if((img2 = SDL_LoadBMP("prout.bmp")) != NULL)
+        {
+			if(img != NULL)
+				SDL_FreeSurface(img);
+				
+			img = img2;
+		}
     
-        if(SDL_BlitSurface(img, NULL, screen, &pos) == -1)
+        if(img && (SDL_BlitSurface(img, NULL, screen, &pos) == -1))
             puts(SDL_GetError());
             
         SDL_Flip(screen);
-        puts("blit");
         SDL_Delay(16);
-    }
+    } while(1);
 
     SDL_Quit();
     
